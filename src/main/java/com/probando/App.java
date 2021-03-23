5e70720a3f6792a7e119e294af2767b98bf3f26a
NR_Alberto <alberto.ruizm@outlook.com>
2021-03-23 00:17:21 +0000

    Initial project version

---
+++ src/main/java/com/probando/App.java
@@ -0,0 +1,13 @@
+package com.probando;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Peoples!" );
+    }
+}
